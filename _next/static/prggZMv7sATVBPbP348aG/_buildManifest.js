608186cc26e19679cc9aafb7c405c0366b907314
Justis-Lamanna <lamanna.justis@gmail.com>
2023-01-02 18:14:50 +0000

    Deploying to gh-pages from @ milomarten/star-warriors-widget@7b5794675a6050f6e560aa01a311029ab2745a53 🚀

--- _next/static/prggZMv7sATVBPbP348aG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7354f9d65b601a74.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
