90e0f4cfb8c40fe701fe906e4f2448965116175b
milomarten <85569415+milomarten@users.noreply.github.com>
2023-07-10 17:25:06 +0000

    Deploying to gh-pages from @ milomarten/star-warriors-widget@6965803a003298ca312115f53bdd41ba8a81b776 🚀

---
+++ _next/static/d8qVAxDa2lijl_y-gvCJ4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/927-372fcf56c2106a14.js","static/chunks/pages/index-e8eb44cecf8f3392.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
